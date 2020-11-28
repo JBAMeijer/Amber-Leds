12a986c46f5ee76a2c2a153596ae852efdd27ba9
Joey Meijer <meijerjoey@gmail.com>
2020-11-28 15:33:00 +0100

    Basic program

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <Arduino.h>
 
+#define RIGHTLEDS 4
+#define MIDDLELEDS 5
+#define LEFTLEDS 6
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(RIGHTLEDS, OUTPUT);
+  pinMode(MIDDLELEDS, OUTPUT);
+  pinMode(LEFTLEDS, OUTPUT);
+
+  delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LEFTLEDS, LOW);
+  digitalWrite(RIGHTLEDS, HIGH);
+  delay(1000);
+  digitalWrite(RIGHTLEDS, LOW);
+  digitalWrite(MIDDLELEDS, HIGH);
+  delay(1000);
+  digitalWrite(MIDDLELEDS, LOW);
+  digitalWrite(LEFTLEDS, HIGH);
+  delay(1000);
 }
